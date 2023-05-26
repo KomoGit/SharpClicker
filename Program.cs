e334f9ef624274f81ddfb5508b667c9f13bcdbec
E.A Nasir <example@example.com>
2023-05-26 10:53:22 +0400

    Updated the code. Parameters are a MUST

--- Program.cs
+++ Program.cs
@@ -4,24 +4,14 @@
     {
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         static extern bool SetCursorPos(int x, int y);
-
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
-
         public const int MOUSEEVENTF_LEFTDOWN = 0x02;
         public const int MOUSEEVENTF_LEFTUP = 0x04;
 
         private static void Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                MouseSimulate(xpos: 108, ypos: 328);
-            }
-            else
-            {
-                MouseSimulate(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]));
-            }
-            Console.ReadLine();
+            MouseSimulate(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]));
         }
         private static void MouseSimulate(int xpos,int ypos)
         {
